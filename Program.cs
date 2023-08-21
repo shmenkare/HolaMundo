3ce95e6454975097bf27f0b7358759c9bd4895b1
“shmenkare” <shmenkare@gmail.com>
2023-08-21 17:24:47 -0400

    HolaMundo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Orion Ra");
+
+            Console.ReadKey();
+        }
+    }
+}
